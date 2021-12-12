ff63e9654abffc3f2418ed6cf7a031b6af470791
jaybee-heon <changheon.whale@gmail.com>
2021-12-13 02:38:40 +0900

    scope

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
+
+  scope :read, -> { where(read: true) }
+  scope :unread, -> { where(read: false) }
 end
--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,5 @@
 class Status < ApplicationRecord
   has_many :users
+
+  scope :active, -> { where(active: true) }
 end
